4bc73706bb24a3d0656261ceb02f7f823f7b6891
JavanRosario <oliveiraajavan@hotmail.com>
2025-11-29 13:51:13 -0300

    consulta de cozinhas

---
+++ src/main/java/com/javanfood/javanfood/jpa/ConsultaCozinhaMain.java
@@ -0,0 +1,25 @@
+package com.javanfood.javanfood.jpa;
+
+import java.util.List;
+
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ApplicationContext;
+
+import com.javanfood.javanfood.JavanfoodApplication;
+import com.javanfood.javanfood.domain.model.Cozinha;
+
+public class ConsultaCozinhaMain {
+	public static void main(String[] args) {
+		ApplicationContext app = new SpringApplicationBuilder(JavanfoodApplication.class).web(WebApplicationType.NONE)
+				.run(args);
+
+		CadastroCozinha cadastroCozinha = app.getBean(CadastroCozinha.class);
+
+		List<Cozinha> cozinhas = cadastroCozinha.listar();
+
+		for (Cozinha c : cozinhas) {
+			System.out.println(c.getNome());
+		}
+	}
+}
