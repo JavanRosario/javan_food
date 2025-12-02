0e4833c1c4b36d422625bd651f0f80e406120218
JavanRosario <oliveiraajavan@hotmail.com>
2025-12-02 11:26:48 -0300

    alga-food

--- src/main/java/com/javanfood/javanfood/domain/model/Cozinha.java
+++ src/main/java/com/javanfood/javanfood/domain/model/Cozinha.java
@@ -1,9 +1,6 @@
 package com.javanfood.javanfood.domain.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
@@ -17,6 +14,7 @@ public class Cozinha {
     @EqualsAndHashCode.Include
 	private Long id;
 
+    @Column(nullable = false)
 	private String nome;
 	
 
