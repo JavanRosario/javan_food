2c1cc8a7ba39d254c182f14053e9eb5650d1ea60
JavanRosario <oliveiraajavan@hotmail.com>
2025-12-06 00:40:10 -0300

    alga-works

--- src/main/java/com/javanfood/javanfood/api/controlers/CozinhaApi.java
+++ src/main/java/com/javanfood/javanfood/api/controlers/CozinhaApi.java
@@ -5,7 +5,10 @@ import com.javanfood.javanfood.api.model.CozinhaXml;
 import com.javanfood.javanfood.domain.model.Cozinha;
 import com.javanfood.javanfood.repository.CozinhaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -37,8 +40,21 @@ public class CozinhaApi {
         return new CozinhaXml(cozinhaRepository.listar());
     }
 
+    //    @ResponseStatus(HttpStatus.CREATED)
     @GetMapping("/{cozinhaId}")
-    public Cozinha buscar(@PathVariable Long cozinhaId) {
-        return cozinhaRepository.findById(cozinhaId);
+    public ResponseEntity<Cozinha> buscar(@PathVariable Long cozinhaId) {
+        Cozinha cozinha = cozinhaRepository.findById(cozinhaId);
+
+//        return ResponseEntity.status(HttpStatus.OK).build();
+
+//        return ResponseEntity.ok(cozinha);
+
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.add(HttpHeaders.LOCATION, "http://localhost:8080/cozinhas");
+
+        return ResponseEntity
+                .status(HttpStatus.FOUND)
+                .headers(httpHeaders)
+                .build();
     }
 }
