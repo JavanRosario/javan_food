d1fe5ce70ce05b235789a3cbad559997cd5c4413
JavanRosario <oliveiraajavan@hotmail.com>
2025-12-09 16:52:13 -0300

    add cadastro de restaurante

---
+++ src/main/java/com/javanfood/javanfood/api/service/CadastroRestauranteService.java
@@ -0,0 +1,32 @@
+package com.javanfood.javanfood.api.service;
+
+import com.javanfood.javanfood.api.repository.CozinhaRepository;
+import com.javanfood.javanfood.api.repository.RestauranteRepository;
+import com.javanfood.javanfood.domain.exeption.EntidadeNaoEncontradaExeption;
+import com.javanfood.javanfood.domain.model.Cozinha;
+import com.javanfood.javanfood.domain.model.Restaurante;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CadastroRestauranteService {
+
+    @Autowired
+    RestauranteRepository restauranteRepository;
+
+    @Autowired
+    CozinhaRepository cozinhaRepository;
+
+    public Restaurante salvar(Restaurante restaurante) {
+        Long cozinhaId = restaurante.getCozinha().getId();
+        Cozinha cozinha = cozinhaRepository.findById(cozinhaId);
+
+        if(cozinha == null){
+            throw new EntidadeNaoEncontradaExeption("Não existe cadastro de cozinha com codigo: "+cozinhaId);
+        }
+
+        restaurante.setCozinha(cozinha);
+
+        return restauranteRepository.adicionar(restaurante);
+    }
+}
