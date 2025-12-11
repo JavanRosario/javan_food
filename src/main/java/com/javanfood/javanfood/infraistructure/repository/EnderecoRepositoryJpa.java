06fcfe97435e103aca7d80aad1727dbca5aaf82c
JavanRosario <oliveiraajavan@hotmail.com>
2025-12-11 08:47:11 -0300

    mudando metodo delete

--- src/main/java/com/javanfood/javanfood/infraistructure/repository/EnderecoRepositoryJpa.java
+++ src/main/java/com/javanfood/javanfood/infraistructure/repository/EnderecoRepositoryJpa.java
@@ -1,10 +1,12 @@
 package com.javanfood.javanfood.infraistructure.repository;
 
+import com.javanfood.javanfood.domain.model.Cidade;
 import com.javanfood.javanfood.domain.model.Endereco;
 import com.javanfood.javanfood.api.repository.EnderecoRespository;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import jakarta.persistence.TypedQuery;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -35,9 +37,12 @@ public class EnderecoRepositoryJpa implements EnderecoRespository {
 
     @Override
     @Transactional
-    public void delete(Endereco cidade) {
-        cidade = findById(cidade.getId());
-        entityManager.remove(cidade);
+    public void delete(Long id) {
+        Endereco endereco = findById(id);
+        if (endereco == null) {
+            throw new EmptyResultDataAccessException(1);
+        }
+        entityManager.remove(endereco);
     }
 
 
