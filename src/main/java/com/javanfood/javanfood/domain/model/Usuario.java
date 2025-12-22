db3adfb21afb0022c088eecd142ede11227f2e86
JavanRosario <oliveiraajavan@hotmail.com>
2025-12-21 23:31:35 -0300

    add entidade uruario

---
+++ src/main/java/com/javanfood/javanfood/domain/model/Usuario.java
@@ -0,0 +1,43 @@
+package com.javanfood.javanfood.domain.model;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Entity
+@Data
+@EqualsAndHashCode
+public class Usuario {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@EqualsAndHashCode.Include
+	private Long id;
+
+	@Column(nullable = false)
+	private String nome;
+	@Column(nullable = false)
+	private String email;
+	@Column(nullable = false)
+	private String senha;
+
+	@Column(nullable = false, name = "datetime")
+	@CreationTimestamp
+	private LocalDateTime dataCadastro;
+
+	@ManyToMany
+	@JoinTable(name = "usuario_grupo", joinColumns = @JoinColumn(name = "usuario_id"), inverseJoinColumns = @JoinColumn(name = "grupo_id"))
+	private List<Grupo> grupos = new ArrayList<>();
+}
