af7db46f64c379da6047165a03a7df7ea82c144a
JavanRosario <oliveiraajavan@hotmail.com>
2025-12-24 12:19:30 -0300

    melhorando o n+1

--- src/main/java/com/javanfood/javanfood/domain/repository/RestauranteRepository.java
+++ src/main/java/com/javanfood/javanfood/domain/repository/RestauranteRepository.java
@@ -3,6 +3,7 @@ package com.javanfood.javanfood.domain.repository;
 import com.javanfood.javanfood.domain.model.Restaurante;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
@@ -13,6 +14,9 @@ import java.util.Optional;
 @Repository
 public interface RestauranteRepository extends CustomJpaRepository<Restaurante, Long>, RestauranteRepositoryQueries, JpaSpecificationExecutor {
     List<Restaurante> findByTaxaFreteBetween(BigDecimal txInicial, BigDecimal txFinal);
+    
+    @Query("from Restaurante r join r.cozinha left join fetch r.formasPagamento")
+    List<Restaurante>findAll();
 
     List<Restaurante> consultarPorNome(@Param("nome") String nome, @Param("id") Long cozinhaId);
 
