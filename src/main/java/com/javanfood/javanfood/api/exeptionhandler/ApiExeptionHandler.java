90b6da56f915bb004a84426f0c51b13ea5d740a9
JavanRosario <oliveiraajavan@hotmail.com>
2026-01-08 08:30:35 -0300

    refactor: herdando de ResponseEntityExceptionHandler

--- src/main/java/com/javanfood/javanfood/api/exeptionhandler/ApiExeptionHandler.java
+++ src/main/java/com/javanfood/javanfood/api/exeptionhandler/ApiExeptionHandler.java
@@ -2,18 +2,21 @@ package com.javanfood.javanfood.api.exeptionhandler;
 
 import java.time.LocalDateTime;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.HttpMediaTypeException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import com.javanfood.javanfood.domain.exeption.EntidadeEmUsoExeption;
 import com.javanfood.javanfood.domain.exeption.EntidadeNaoEncontradaExeption;
 import com.javanfood.javanfood.domain.exeption.NegocioExeption;
 
 @ControllerAdvice
-public class ApiExeptionHandler {
+public class ApiExeptionHandler extends ResponseEntityExceptionHandler {
 
 	@ExceptionHandler(EntidadeNaoEncontradaExeption.class)
 	public ResponseEntity<?> tratarEstadoNaoEncontradoExeption(EntidadeNaoEncontradaExeption e) {
@@ -33,14 +36,6 @@ public class ApiExeptionHandler {
 		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(problema);
 	}
 
-	@ExceptionHandler(HttpMediaTypeException.class)
-	public ResponseEntity<?> tratarHttpMediaTypeException() {
-		Problema problema = Problema.builder()
-				.dataHora(LocalDateTime.now())
-				.menssagem("O tipo de mídia não é aceito")
-				.build();
-		return ResponseEntity.status(HttpStatus.UNSUPPORTED_MEDIA_TYPE).body(problema);
-	}
 
 	@ExceptionHandler(EntidadeEmUsoExeption.class)
 	public ResponseEntity<?> tratarEntidadeEmUsoExeption(EntidadeEmUsoExeption e) {
@@ -49,6 +44,20 @@ public class ApiExeptionHandler {
 				.menssagem(e.getMessage())
 				.build();
 		return ResponseEntity.status(HttpStatus.CONFLICT).body(problema);
+	}
+
+	@Override
+	protected ResponseEntity<Object> handleExceptionInternal(
+			Exception ex,
+			Object body,
+			HttpHeaders headers,
+			HttpStatusCode statusCode,
+			WebRequest request) {
+		body = Problema.builder()
+				.dataHora(LocalDateTime.now())
+				.menssagem(ex.getMessage())
+				.build();
+		return super.handleExceptionInternal(ex, body, headers, statusCode, request);
 	}
 
 
