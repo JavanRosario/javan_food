02d193ebc9dfcc203fe74081b941ae342b0467d3
JavanRosario <oliveiraajavan@hotmail.com>
2026-01-08 08:31:05 -0300

    refactor: removendo @responsestatus

--- src/main/java/com/javanfood/javanfood/domain/exeption/EntidadeNaoEncontradaExeption.java
+++ src/main/java/com/javanfood/javanfood/domain/exeption/EntidadeNaoEncontradaExeption.java
@@ -1,9 +1,5 @@
 package com.javanfood.javanfood.domain.exeption;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(HttpStatus.NOT_FOUND)
 public abstract class EntidadeNaoEncontradaExeption extends NegocioExeption {
     public EntidadeNaoEncontradaExeption(String message) {
         super(message);
